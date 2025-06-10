b71713ba26bd5146a72c2c329976d6e740368478
Manech <chenamgg@gmail.com>
2025-06-10 11:47:02 +0200

    correction bug pubspec

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:chaudiere_mobile/chaudiere_app.dart';
 import 'package:flutter/material.dart';
-import 'chaudiere_app.dart';
-import 'package:chaudiere_mobile/models/evenement_provider.dart';
 
 void main() {
   runApp(const ChaudiereApp());
