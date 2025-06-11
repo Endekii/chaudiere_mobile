e5578a71b1766dd0d84f3410ff64df5ad8bc78b4
Kevin Ngo <kevin.ngo8@etu.univ-lorraine.fr>
2025-06-11 10:43:08 +0200

    update events_master

--- lib/screens/events_master.dart
+++ lib/screens/events_master.dart
@@ -1,6 +1,21 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:chaudiere_mobile/models/event.dart';
-import 'package:chaudiere_mobile/services/event_service.dart';
+
+Future<Event> fetchEvent() async {
+  final response = await http.get(
+    Uri.parse('http://docketu.iutnc.univ-lorraine.fr:13000/api/evenements/1'),
+  );
+
+  if (response.statusCode == 200) {
+    print(response.body);
+    return Event.fromJson(jsonDecode(response.body) as Map<String, dynamic>);
+  } else {
+    throw Exception('Failed to load event');
+  }
+}
 
 class EventsMaster extends StatefulWidget {
   const EventsMaster({super.key});
@@ -15,21 +30,32 @@ class _EventsMasterState extends State<EventsMaster> {
   @override
   void initState(){
     super.initState();
-    futureEvent = EventService().fetchEvent();
+    futureEvent = fetchEvent();
   }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: FutureBuilder<Event>(
-          future: futureEvent,
-          builder: (context, snapshot){
-            if(snapshot.hasData){
+    return MaterialApp(
+      title: 'Chaudière',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Liste des événements')),
+        body: Center(
+          child: FutureBuilder<Event>(
+            future: futureEvent,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.titre);
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-            } else {
-              return Text('${snapshot.error}');
-            }
-            return const CircularProgressIndicator();
-          },
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
       ),
     );
   }
